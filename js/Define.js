b1f12e661b342d15290a360740d2072e6105f4ff
ntude <goxluix7@yahoo.com.tw>
2016-04-08 12:36:06 +0800

    d

--- js/Define.js
+++ js/Define.js
@@ -2,7 +2,7 @@
 			function adjustment() {
 				var a = $("#backPng").width();
 				
-				document.getElementById('backPng').style.height = (a * 1.78 ) + "px";
+				document.getElementById('backPng').style.width = (a * 1.78 ) + "px";
 				/*document.getElementById('backPng').style.marginLeft = -(a * 0.5625 / 2) + "px";*/
 				/* iphone6: width / height = 0.5625 */
 				/* marginLeft -> make it center */
