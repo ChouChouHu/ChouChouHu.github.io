3588df0ff315df5bfcbf1fbb69d81ceacf0a26fc
ntude <goxluix7@yahoo.com.tw>
2016-04-08 11:52:48 +0800

    asdf

--- js/Define.js
+++ js/Define.js
@@ -1,20 +1,18 @@
 			/* height adjustment to all device */
 			function adjustment() {
 				var a = $("#backPng").height();
-				/*
+				
 				document.getElementById('backPng').style.width = (a * 0.5625 ) + "px";
 				document.getElementById('backPng').style.marginLeft = -(a * 0.5625 / 2) + "px";
-				*/
+				
 				/* iphone6: width / height = 0.5625 */
 				/* marginLeft -> make it center */
 
 				/* font-size */
 				document.getElementById('backPng').style.fontSize = (a / 582) + "em";
 				document.getElementById('backPng').style.lineHeight = 1.4 + "em";
-				
 				$(".memberZone").css("font-size", 0.87 + "em");
 				$(".memberZone").css("line-height", 1.3 + "em");
-
 				$("#commitment").css("font-size", 0.6 + "em");
 				$("#commitment").css("line-height", 1.3 + "em");
 				$("#sendCode").css("height", 1.4 + "em");
