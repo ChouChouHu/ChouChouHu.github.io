878cff4570b99f1bd19deaf101f26d783d7e39d4
ntude <goxluix7@yahoo.com.tw>
2017-02-03 15:59:07 +0800

    rwd

--- picnese2/js/index.js
+++ picnese2/js/index.js
@@ -1,29 +1,57 @@
 
 
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// 	$(".treetop").click(function(){
-// 		$(".chouWindowContent iframe").attr("src", "PicneseEra.html");
-// 		$(".chouWindow").fadeIn();
-// 	});
+	n = $(window).height();
+	if( n > 480 ) {
+		$(".tree").css("transform", "scale(" + ( 1 - ( ( 652 - n ) / 840 ) ) + ")");
+		$(".tree").css("height", ( 100 + ( (652 - n) / 10.7) )+ "vh");
+		$(".tree").css("bottom", ( (n - 652) / 20) + "vh");
+		$(".tree").css("position", "absolute");
+	}
+	else {
+		$(".tree").removeAttr("style");
+		$(".tree").css("position", "initial");
+	}
 
-// 	$(".cooperation").click(function(){
-// 		$(".chouWindowContent iframe").attr("src", "cooperation.html");
-// 		$(".chouWindow").fadeIn();
-// 	});
+	$(window).resize(function() {
+        n = $(window).height();
+        // alert(n);
+        if( n > 480 ) {
+	      	$(".tree").css("transform", "scale(" + ( 1 - ( ( 652 - n ) / 840 ) ) + ")");
+	      	$(".tree").css("height", ( 100 + ( (652 - n) / 10.7) )+ "vh");
+	      	$(".tree").css("bottom", ( (n - 652) / 20) + "vh");
+	      	$(".tree").css("position", "absolute");
+	    }
+		else {
+			$(".tree").removeAttr("style");
+			$(".tree").css("position", "initial");
+		}
+    });
 
-// 	$(".guess").click(function(){
-// 		$(".chouWindowContent iframe").attr("src", "guess.html");
-// 		$(".chouWindow").fadeIn();
-// 	});
+	// fancybox
+	// $(".treetop").click(function(){
+	// 	$(".chouWindowContent iframe").attr("src", "PicneseEra.html");
+	// 	$(".chouWindow").fadeIn();
+	// });
 
-// 	$(".tableGame").click(function(){
-// 		$(".chouWindowContent iframe").attr("src", "tableGame.html");
-// 		$(".chouWindow").fadeIn();
-// 	});
+	// $(".cooperation").click(function(){
+	// 	$(".chouWindowContent iframe").attr("src", "cooperation.html");
+	// 	$(".chouWindow").fadeIn();
+	// });
 
-// 	$(".chouWindow").click(function(){
-// 		$(this).css("display", "none");
-// 	});
-// });
+	// $(".guess").click(function(){
+	// 	$(".chouWindowContent iframe").attr("src", "guess.html");
+	// 	$(".chouWindow").fadeIn();
+	// });
+
+	// $(".tableGame").click(function(){
+	// 	$(".chouWindowContent iframe").attr("src", "tableGame.html");
+	// 	$(".chouWindow").fadeIn();
+	// });
+
+	// $(".chouWindow").click(function(){
+	// 	$(this).css("display", "none");
+	// });
+});
 
