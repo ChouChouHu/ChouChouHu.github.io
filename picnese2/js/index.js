de8a6fcdaa9231dbdcc04a3a52411b541f58e5a6
ntude <goxluix7@yahoo.com.tw>
2017-02-03 14:54:49 +0800

    new picnese 2

--- picnese2/js/index.js
+++ picnese2/js/index.js
@@ -1,29 +1,29 @@
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	$(".treetop").click(function(){
-		$(".chouWindowContent iframe").attr("src", "PicneseEra.html");
-		$(".chouWindow").fadeIn();
-	});
+// 	$(".treetop").click(function(){
+// 		$(".chouWindowContent iframe").attr("src", "PicneseEra.html");
+// 		$(".chouWindow").fadeIn();
+// 	});
 
-	$(".cooperation").click(function(){
-		$(".chouWindowContent iframe").attr("src", "cooperation.html");
-		$(".chouWindow").fadeIn();
-	});
+// 	$(".cooperation").click(function(){
+// 		$(".chouWindowContent iframe").attr("src", "cooperation.html");
+// 		$(".chouWindow").fadeIn();
+// 	});
 
-	$(".guess").click(function(){
-		$(".chouWindowContent iframe").attr("src", "guess.html");
-		$(".chouWindow").fadeIn();
-	});
+// 	$(".guess").click(function(){
+// 		$(".chouWindowContent iframe").attr("src", "guess.html");
+// 		$(".chouWindow").fadeIn();
+// 	});
 
-	$(".tableGame").click(function(){
-		$(".chouWindowContent iframe").attr("src", "tableGame.html");
-		$(".chouWindow").fadeIn();
-	});
+// 	$(".tableGame").click(function(){
+// 		$(".chouWindowContent iframe").attr("src", "tableGame.html");
+// 		$(".chouWindow").fadeIn();
+// 	});
 
-	$(".chouWindow").click(function(){
-		$(this).css("display", "none");
-	});
-});
+// 	$(".chouWindow").click(function(){
+// 		$(this).css("display", "none");
+// 	});
+// });
 
