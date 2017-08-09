c436b19782ef643b3018fa28e5bc2738116eb591
ntude <goxluix7@yahoo.com.tw>
2017-08-09 23:59:14 +0800

    menu complete

--- Rethink2027/js/bar.js
+++ Rethink2027/js/bar.js
@@ -19,4 +19,36 @@ $(document).ready(function(){
 		// children for one layer; find for all child
 	})
 
+	$("#btnNews").hover(function(){
+		$("#picNews").css("display", "block");
+	}, function(){
+		$("#picNews").css("display", "none");
+	})
+	$("#btnAbout").hover(function(){
+		$("#picAbout").css("display", "block");
+	}, function(){
+		$("#picAbout").css("display", "none");
+	})
+	$("#btnActivity").hover(function(){
+		$("#picActivity").css("display", "block");
+	}, function(){
+		$("#picActivity").css("display", "none");
+	})
+	$("#btnWall").hover(function(){
+		$("#picWall").css("display", "block");
+	}, function(){
+		$("#picWall").css("display", "none");
+	})
+	$("#btnPartner").hover(function(){
+		$("#picPartner").css("display", "block");
+	}, function(){
+		$("#picPartner").css("display", "none");
+	})
+	$("#btnDaily").hover(function(){
+		$("#picDaily").css("display", "block");
+	}, function(){
+		$("#picDaily").css("display", "none");
+	})
+
+
 });
