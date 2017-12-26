d764cf583a4ba9b4b12c1c3d8f7d5d7e6cf11fb6
ntude <goxluix7@yahoo.com.tw>
2017-12-26 19:57:39 +0800

    style.css

--- picnese/js/forum_each.js
+++
@@ -1,6 +0,0 @@
-	$(document).ready(function(){
-		$(".leave > .img").click(function(){
-			$(this).find(".before").css("display", "none");
-			$(this).find(".after").css("display", "block");
-		});
-	});
