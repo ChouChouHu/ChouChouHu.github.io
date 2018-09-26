e2e6115d4bea33aae93eae4b7a753232a658abbe
saderbd <goxluix7@ntu.im>
2018-09-26 17:31:51 +0800

    rookie

--- Sara/js/index.js
+++ Sara/js/index.js
@@ -4,11 +4,18 @@ $(document).ready(function(){
 	
 	
 
-	// $(".sound").click(function(){
-	// 	// alert("f");
-	// 	var boolie = $(".bgsound").prop("muted");
- //    	$(".bgsound").prop("muted",!boolie);
-	// })
+	$(".sound").click(function(){
+		// alert("f");
+		var boolie = $(".bgsound").prop("muted");
+    	$(".bgsound").prop("muted",!boolie);
+
+    	if ($(".bgsound").prop("muted") == true) {
+    		alert("ture");
+    	}
+    	else {
+    		alert("false");
+    	}
+	})
 
 
 });
